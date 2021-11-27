91f4d39aaedda0053556f4822c9086094515e437
Zxilly <zhouxinyu1001@gmail.com>
2021-11-27 23:44:57 +0800

    feat: complete sdk
    Difficult to implement unit test. Thanks fucking CoPilot
    
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>

--- sdk/src/main/java/top/learningman/hystime/sdk/HystimeClient.kt
+++ sdk/src/main/java/top/learningman/hystime/sdk/HystimeClient.kt
@@ -1,16 +1,25 @@
 package top.learningman.hystime.sdk
 
+import TargetCreateMutation
+import TargetDeleteMutation
 import TargetQuery
 import TargetTimePiecesQuery
+import TargetUpdateMutation
+import TimePieceCreateMutation
+import TimePieceDeleteMutation
+import TimePieceUpdateMutation
+import TimePiecesCreateForTargetMutation
+import UserCreateMutation
 import UserInfoQuery
 import UserTargetsQuery
 import UserTimePiecesQuery
+import UserUpdateMutation
 import android.util.Log
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.Input
 import com.apollographql.apollo.coroutines.await
 import okhttp3.OkHttpClient
-import type.CustomType
+import type.*
 import java.util.*
 
 class HystimeClient(endpoint: String, authCode: String) {
@@ -73,11 +82,11 @@ class HystimeClient(endpoint: String, authCode: String) {
 
     @Suppress("MemberVisibilityCanBePrivate")
     suspend fun getTargetTimePieces(
-        targetId: String,
+        targetID: String,
         first: Int,
-        after: Input<String>
+        after: Input<String> = Input.absent()
     ): TargetTimePiecesQuery.TimePieces? {
-        val resp = client.query(TargetTimePiecesQuery(targetId, first, after)).await()
+        val resp = client.query(TargetTimePiecesQuery(targetID, first, after)).await()
         resp.data?.target?.let {
             return it.timePieces
         }
@@ -89,7 +98,7 @@ class HystimeClient(endpoint: String, authCode: String) {
     suspend fun getUserTimePieces(
         userID: String,
         first: Int,
-        after: Input<String>
+        after: Input<String> = Input.absent()
     ): UserTimePiecesQuery.Timepieces? {
         val resp = client.query(UserTimePiecesQuery(userID, first, after)).await()
         resp.data?.let {
@@ -111,7 +120,7 @@ class HystimeClient(endpoint: String, authCode: String) {
                 }
                 for (nd in timePieces.edges) {
                     nd.node.let {
-                        if (it.start.inAWeek()) {
+                        if (it.start.inPastWeek()) {
                             pieces.add(nd.node)
                         } else {
                             return pieces.toList()
@@ -127,18 +136,18 @@ class HystimeClient(endpoint: String, authCode: String) {
     }
 
     suspend fun getLastWeekTargetTimePieces(
-        targetId: String
+        targetID: String
     ): List<TargetTimePiecesQuery.Node> {
         val pieces = mutableListOf<TargetTimePiecesQuery.Node>()
         var cursor: Input<String> = Input.absent()
         while (true) {
-            getTargetTimePieces(targetId, 30, cursor)?.let { timePieces ->
+            getTargetTimePieces(targetID, 30, cursor)?.let { timePieces ->
                 if (timePieces.totalCount == 0) {
                     return pieces.toList()
                 }
                 for (nd in timePieces.edges) {
                     nd.node.let {
-                        if (it.start.inAWeek()) {
+                        if (it.start.inPastWeek()) {
                             pieces.add(nd.node)
                         } else {
                             return pieces.toList()
@@ -152,6 +161,124 @@ class HystimeClient(endpoint: String, authCode: String) {
             } ?: return pieces.toList()
         }
     }
+
+    suspend fun createUser(username: String): UserCreateMutation.UserCreate? {
+        val input = UserCreateInput(username)
+        val resp = client.mutate(UserCreateMutation(input)).await()
+        resp.data?.let {
+            return it.userCreate
+        }
+        Log.e("createUser", resp.errors.toString())
+        return null
+    }
+
+    suspend fun updateUser(
+        userID: String,
+        username: Input<String>
+    ): UserUpdateMutation.UserUpdate? {
+        val input = UserUpdateInput(username)
+        val resp = client.mutate(UserUpdateMutation(userID, input)).await()
+        resp.data?.let {
+            return it.userUpdate
+        }
+        Log.e("updateUser", resp.errors.toString())
+        return null
+    }
+
+    suspend fun createTarget(
+        userID: String,
+        name: String,
+        timeSpent: Input<Int>,
+        type: Input<TargetType>
+    ): TargetCreateMutation.TargetCreate? {
+        val input = TargetCreateInput(name, timeSpent, type)
+        val resp = client.mutate(TargetCreateMutation(userID, input)).await()
+        resp.data?.let {
+            return it.targetCreate
+        }
+        Log.e("createTarget", resp.errors.toString())
+        return null
+    }
+
+    suspend fun updateTarget(
+        targetID: String,
+        name: Input<String>,
+        timeSpent: Input<Int>,
+        type: Input<TargetType>
+    ): TargetUpdateMutation.TargetUpdate? {
+        val input = TargetUpdateInput(name, timeSpent, type)
+        val resp = client.mutate(TargetUpdateMutation(targetID, input)).await()
+        resp.data?.let {
+            return it.targetUpdate
+        }
+        Log.e("updateTarget", resp.errors.toString())
+        return null
+    }
+
+    suspend fun deleteTarget(
+        targetID: String
+    ): Boolean {
+        val resp = client.mutate(TargetDeleteMutation(targetID)).await()
+        resp.data?.let {
+            return it.targetDelete
+        }
+        Log.e("deleteTarget", resp.errors.toString())
+        return false
+    }
+
+    suspend fun createTimePiece(
+        targetID: String,
+        start: Date,
+        duration: Int,
+        type: Input<TimePieceType>
+    ): TimePieceCreateMutation.TimePieceCreate? {
+        val input = TimePieceCreateInput(start, duration, type)
+        val resp = client.mutate(TimePieceCreateMutation(targetID, input)).await()
+        resp.data?.let {
+            return it.timePieceCreate
+        }
+        Log.e("createTimePiece", resp.errors.toString())
+        return null
+    }
+
+    suspend fun updateTimePiece(
+        timePieceID: Int,
+        start: Input<Date>,
+        duration: Input<Int>,
+        type: Input<TimePieceType>
+    ): TimePieceUpdateMutation.TimePieceUpdate? {
+        val input = TimePieceUpdateInput(start, duration, type)
+        val resp = client.mutate(TimePieceUpdateMutation(timePieceID, input)).await()
+        resp.data?.let {
+            return it.timePieceUpdate
+        }
+        Log.e("updateTimePiece", resp.errors.toString())
+        return null
+    }
+
+    suspend fun deleteTimePiece(
+        timePieceID: Int
+    ): Boolean {
+        val resp = client.mutate(TimePieceDeleteMutation(timePieceID)).await()
+        resp.data?.let {
+            return it.timePieceDelete
+        }
+        Log.e("deleteTimePiece", resp.errors.toString())
+        return false
+    }
+
+    suspend fun createTimePiecesForTarget(
+        targetID: String,
+        inputs: List<TimePieceCreateInput>
+    ): List<TimePiecesCreateForTargetMutation.TimePiecesCreateForTarget>? {
+        val resp = client.mutate(TimePiecesCreateForTargetMutation(targetID, inputs)).await()
+        resp.data?.let {
+            return it.timePiecesCreateForTarget
+        }
+        Log.e("createTimePiecesForTarget", resp.errors.toString())
+        return null
+    }
+
 }
 
 // Check if url is valid and safe
@@ -161,13 +288,13 @@ fun String.isUrl(): Boolean {
 }
 
 // Check if date in the past week
-fun Date.inAWeek(): Boolean {
+fun Date.inPastWeek(): Boolean {
     val cal = Calendar.getInstance()
     cal.time = this
-    cal.set(Calendar.HOUR_OF_DAY, 0);
-    cal.set(Calendar.MINUTE, 0);
-    cal.set(Calendar.SECOND, 0);
-    cal.set(Calendar.MILLISECOND, 0);
+    cal.set(Calendar.HOUR_OF_DAY, 0)
+    cal.set(Calendar.MINUTE, 0)
+    cal.set(Calendar.SECOND, 0)
+    cal.set(Calendar.MILLISECOND, 0)
     cal.add(Calendar.DAY_OF_YEAR, 7)
     return cal.time.after(Date())
 }
