c046062982d1a2fee8785c63e8af52d484b6f0f5
Zxilly <zhouxinyu1001@gmail.com>
2021-12-10 12:10:24 +0800

    feat: enhance status

--- app/src/main/java/top/learningman/hystime/ui/timer/TimerViewModel.kt
+++ app/src/main/java/top/learningman/hystime/ui/timer/TimerViewModel.kt
@@ -1,14 +1,16 @@
 package top.learningman.hystime.ui.timer
 
-class TimerViewModel {
-    enum class TimerStatus{
-        WAIT_START,
-        WORK_RUNNING,
-        WORK_PAUSE,
-        WORK_FINISH_NORMAL,
-        WORK_FINISH_SHORT,
-        WORK_FINISH_LONG,
-        BREAK_SKIP,
-        BREAK_FINISH
+import androidx.lifecycle.ViewModel
+
+class TimerViewModel : ViewModel() {
+    enum class TimerStatus {
+        WAIT_START, // wait_start
+        WORK_RUNNING, // work_running
+        WORK_PAUSE, // work_pause
+        WORK_FINISH_SHORT, // work_finish(short)
+        WORK_FINISH_LONG, // work_finish(long)
+        BREAK_RUNNING, // break_running
+        BREAK_FINISH_LONG, // break_finish(long)
+        BREAK_FINISH_SHORT // break_finish(short)
     }
 }
