0b3440d550afa8660b4fff825f6b7dc84226ef54
Zxilly <zhouxinyu1001@gmail.com>
2022-02-04 13:09:53 +0800

    feat: auto switch timeSpent appearance

--- app/src/main/java/top/learningman/hystime/ui/dashboard/ui/DashboardFragment.kt
+++ app/src/main/java/top/learningman/hystime/ui/dashboard/ui/DashboardFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textview.MaterialTextView
+import top.learningman.hystime.R
 import top.learningman.hystime.databinding.FragmentDashboardBinding
 import top.learningman.hystime.ui.dashboard.DashboardActivity
 
@@ -32,18 +34,45 @@ class DashboardFragment : Fragment() {
             requireArguments().getSerializable(FRAGMENT_DATA_KEY) as DashboardActivity.Statistic
         binding.todayPomodoro.text = data.todayPomodoroCount.toString()
         binding.totalPomodoro.text = data.pomodoroCount.toString()
-        binding.todayFocusLength.text = data.todayTimeSpent.toTimeStr()
-        binding.totalFocusLength.text = data.timeSpent.toTimeStr()
 
+        // Inflate ViewStub
+        mapOf(
+            data.timeSpent to binding.totalFocusLength,
+            data.todayTimeSpent to binding.todayFocusLength
+        ).forEach {
+            val time = it.key.toTime()
+            val vs = it.value
+            if (time.minuteOnly()) {
+                vs.layoutResource = R.layout.item_dashboard_min_text
+                val v = vs.inflate()
+                val min: MaterialTextView = v.findViewById(R.id.minute)
+                min.text = time.minute.toString()
+            } else {
+                val v = vs.inflate()
+                val min: MaterialTextView = v.findViewById(R.id.minute)
+                val hour: MaterialTextView = v.findViewById(R.id.hour)
+                min.text = time.minute.toString()
+                hour.text = time.hour.toString()
+            }
+        }
     }
 
     companion object {
         const val FRAGMENT_DATA_KEY = "fragment_data_key"
 
-        fun Int.toTimeStr(): String {
+        data class Time(
+            val hour: Int = 0,
+            val minute: Int
+        ) {
+            fun minuteOnly(): Boolean {
+                return hour == 0
+            }
+        }
+
+        fun Int.toTime(): Time {
             val hour = this / 3600
             val minute = this % 3600 / 60
-            return "%02d:%02d".format(hour, minute)
+            return Time(hour, minute)
         }
     }
 
